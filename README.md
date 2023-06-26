4244926e6931930619f4947d5e0491f26483a30a
pcgofpa <pcgofpa@gmail.com>
2023-06-26 07:14:54 -0500

    0712
    - Added requirements to the README
    - Started mapping the program via comments.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Higher_Lower_Game
  
+<ul>
+	<li>Call two random celebrities from data dictionary.</li>
+	<li>Display name and description</li>
+	<li>Compare follower_count for both objects</li>
+</ul>
---
+++ main.py
@@ -0,0 +1,16 @@
+# Higher Lower game guess who has more Instagram followers
+# imports
+import random
+from art import logo, vs
+from game_data import data
+
+# Global Variables
+user_score = 0
+is_game_over = False
+
+# Call data for two random entries in data.
+
+
+# def function to compare a vs b. return highest follower count.
+
+# user guess
